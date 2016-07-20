a23f1d83ef8b1d9e7e9912d2d2927c36ab47e791
Aditya Ekbote <ekbote.aditya@gmail.com>
2016-07-20 14:41:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
-
+var title;
 request('http://uk.flightaware.com/live/flight/DAL4201', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
-    $('#polling-flight_status > div.track-panel-inner > table > tbody > tr:nth-child(1) > td').each(function(i, element){
-      var a = $(this);
-      console.log(a.text());
-    });
+    title = $('#polling-flight_status > div.track-panel-inner > table > tbody > tr:nth-child(1) > td').text();
+    console.log(title);
   }
 });
 
