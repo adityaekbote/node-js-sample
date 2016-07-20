5e26dc58f39cbdf2371cc544c0944834873ab2b3
Aditya Ekbote <ekbote.aditya@gmail.com>
2016-07-20 12:16:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 var express = require('express')
+var request = require('request');
+var cheerio = require('cheerio');
 var app = express()
+
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+   url = 'https://www.google.co.in/search?q=dl1162';
+  request(url, function (error, response, body) 
+{  
+  
+  if (!error && response.statusCode == 200) 
+  {
+    var $ = cheerio.load(body);
+    var title = $("#rso > div.g.tpo.knavi.obcontainer.mod > div > div.card-section.vk_c > ol > li > div > span").text();
+  }
 })
+  response.send(title);
+})
+
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
